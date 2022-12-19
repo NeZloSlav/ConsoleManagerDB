b9bf0c8522c6cf3051f8288ffe24c4f240900803
Viacheslav_Nakhodnov <nahodnoff.vyacheslaw@yandex.ru>
2022-12-19 15:33:01 +0300

    Создал класс DataActions, который будет хранить в себе все манипуляции с пользователями.

--- ConsoleManagerDB/Program.cs
+++ ConsoleManagerDB/Program.cs
@@ -139,177 +139,184 @@ namespace ConsoleManagerDB
 
     }
 
-    internal class Program
+    internal class DataActoins
     {
-        static void Main(string[] args)
+        public class ViaQuery
         {
-            //string connectionString = @"Server=.\SQLEXPRESS;Database=userdb;Trusted_Connection=True";
-
-            //SqlConnection connection = new SqlConnection(connectionString);
-
-            //connection.Open();
-            //Console.WriteLine("Подключение открыто");
-
-
-
-
-            //connection.Close();
-            //Console.WriteLine("Подключение закрыто...");
-
-            int id = InputUserData.UserID();
-            Console.WriteLine(id);
-        }
-
-        private static List<User> GetUsersList(SqlConnection connection)
-        {
-            List<User> users = new List<User>();
-
-            SqlCommand command = new SqlCommand
+            public static List<User> GetUsersList(SqlConnection connection)
             {
-                CommandText = "SELECT * FROM Users",
-                Connection = connection
-            };
+                List<User> users = new List<User>();
 
-            SqlDataReader reader = command.ExecuteReader();
-
-            if (reader.HasRows)
-            {
-                while (reader.Read())
+                SqlCommand command = new SqlCommand
                 {
-                    int Id = reader.GetInt32(0);
-                    string Name = reader.GetString(1);
-                    int Age = reader.GetInt32(2);
+                    CommandText = "SELECT * FROM Users",
+                    Connection = connection
+                };
 
-                    User user = new User(Id, Name, Age);
-                    users.Add(user);
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        int Id = reader.GetInt32(0);
+                        string Name = reader.GetString(1);
+                        int Age = reader.GetInt32(2);
+
+                        User user = new User(Id, Name, Age);
+                        users.Add(user);
+                    }
                 }
+                reader.Close();
+
+                Console.WriteLine("Список пользователей создан.");
+
+                return users;
             }
-            reader.Close();
 
-            Console.WriteLine("Список пользователей создан.");
-
-            return users;
-        }
-
-        static void AddUser(SqlConnection connection)
-        {
-            string name = InputUserData.UserName();
-
-            int age = InputUserData.UserAge();
-
-            SqlCommand command = new SqlCommand
+            public static void AddUser(SqlConnection connection)
             {
-                CommandText = "INSERT INTO Users (Name, Age) VALUES (@name, @age)",
-                Connection = connection
-            };
+                string name = InputUserData.UserName();
 
-            SqlParameter[] parameters =
-            {
+                int age = InputUserData.UserAge();
+
+                SqlCommand command = new SqlCommand
+                {
+                    CommandText = "INSERT INTO Users (Name, Age) VALUES (@name, @age)",
+                    Connection = connection
+                };
+
+                SqlParameter[] parameters =
+                {
                 new SqlParameter("@name", name),
                 new SqlParameter("@age", age)
             };
 
-            command.Parameters.Add(parameters);
-            int lines = command.ExecuteNonQuery();
+                command.Parameters.Add(parameters);
+                int lines = command.ExecuteNonQuery();
 
-            if (lines > 0)
-            {
-                Console.WriteLine("Успешно добавлено! Затронуто ({0}) строк", lines);
-            }
-        }
-
-        static void DeleteUser(SqlConnection connection)
-        {
-            int id = InputUserData.UserID();
-
-            SqlCommand command = new SqlCommand
-            {
-                CommandText = "DELETE Users WHERE Id = @id",
-                Connection = connection
-            };
-
-            SqlParameter idParam = new SqlParameter("@id", id);
-            command.Parameters.Add(idParam);
-
-            int lines = command.ExecuteNonQuery();
-
-            if (lines > 0)
-            {
-                Console.WriteLine("Успешно! Затронуто строк ({0})", lines);
-            }
-            else
-            {
-                Console.WriteLine("Пользователя с id = {0} не существует", id);
+                if (lines > 0)
+                {
+                    Console.WriteLine("Успешно добавлено! Затронуто ({0}) строк", lines);
+                }
             }
 
-        }
-
-        class UpdateUser
-        {
-            private static readonly int ID = InputUserData.UserID();
-
-            private static void ForName(SqlConnection connection)
+            public static void DeleteUser(SqlConnection connection)
             {
-                string Name = InputUserData.UserName();
+                int id = InputUserData.UserID();
 
                 SqlCommand command = new SqlCommand
                 {
-                    CommandText = "UPDATE Users SET Name = @name WHERE Id = @id",
+                    CommandText = "DELETE Users WHERE Id = @id",
                     Connection = connection
                 };
 
-                SqlParameter[] parameters =
+                SqlParameter idParam = new SqlParameter("@id", id);
+                command.Parameters.Add(idParam);
+
+                int lines = command.ExecuteNonQuery();
+
+                if (lines > 0)
                 {
+                    Console.WriteLine("Успешно! Затронуто строк ({0})", lines);
+                }
+                else
+                {
+                    Console.WriteLine("Пользователя с id = {0} не существует", id);
+                }
+
+            }
+
+            public class UpdateUser
+            {
+                private static readonly int ID = InputUserData.UserID();
+
+                public static void ForName(SqlConnection connection)
+                {
+                    string Name = InputUserData.UserName();
+
+                    SqlCommand command = new SqlCommand
+                    {
+                        CommandText = "UPDATE Users SET Name = @name WHERE Id = @id",
+                        Connection = connection
+                    };
+
+                    SqlParameter[] parameters =
+                    {
                     new SqlParameter("@name", Name),
                     new SqlParameter("@id", ID)
                 };
 
-                command.Parameters.Add(parameters);
+                    command.Parameters.Add(parameters);
 
-                int lines = command.ExecuteNonQuery();
+                    int lines = command.ExecuteNonQuery();
 
-                if (lines > 0)
-                {
-                    Console.WriteLine("Данные успешно обновлены!");
+                    if (lines > 0)
+                    {
+                        Console.WriteLine("Данные успешно обновлены!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Данные не обновлены. Пользователя с данным id не существует...");
+                    }
                 }
-                else
-                {
-                    Console.WriteLine("Данные не обновлены. Пользователя с данным id не существует...");
-                }
-            }
 
-            private static void ForAge(SqlConnection connection)
-            {
-                int Age = InputUserData.UserAge();
-
-                SqlCommand command = new SqlCommand
+                public static void ForAge(SqlConnection connection)
                 {
-                    CommandText = "UPDATE Users SET Age = @age WHERE Id = @id",
-                    Connection = connection
-                };
+                    int Age = InputUserData.UserAge();
 
-                SqlParameter[] parameters =
-                {
+                    SqlCommand command = new SqlCommand
+                    {
+                        CommandText = "UPDATE Users SET Age = @age WHERE Id = @id",
+                        Connection = connection
+                    };
+
+                    SqlParameter[] parameters =
+                    {
                     new SqlParameter("@age", Age),
                     new SqlParameter("@id", ID)
                 };
 
-                command.Parameters.Add(parameters);
+                    command.Parameters.Add(parameters);
 
-                int lines = command.ExecuteNonQuery();
+                    int lines = command.ExecuteNonQuery();
 
-                if (lines > 0)
-                {
-                    Console.WriteLine("Данные успешно обновлены!");
+                    if (lines > 0)
+                    {
+                        Console.WriteLine("Данные успешно обновлены!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Данные не обновлены. Пользователя с данным id не существует.");
+                    }
                 }
-                else
-                {
-                    Console.WriteLine("Данные не обновлены. Пользователя с данным id не существует.");
-                }
+
             }
-
         }
 
+        public class ViaProcedure
+        {
+
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Server=.\SQLEXPRESS;Database=userdb;Trusted_Connection=True";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            connection.Open();
+            Console.WriteLine("Подключение открыто");
+
+
+
+            connection.Close();
+            Console.WriteLine("Подключение закрыто...");
+
+        }
     }
 
 
